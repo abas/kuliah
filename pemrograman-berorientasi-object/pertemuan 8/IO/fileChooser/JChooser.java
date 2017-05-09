38461ab057217268b5a268d6a029132b2ed48434
ahmadbasir <akhmadbasir5@gmail.com>
2017-05-10 03:53:39 +0700

    pembaruan materi dan tugas PBO pertemuan 8

--- pemrograman-berorientasi-object/pertemuan 8/IO/fileChooser/JChooser.java
+++ pemrograman-berorientasi-object/pertemuan 8/IO/fileChooser/JChooser.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import javax.swing.*;
 
-class JChoosers{
+class JChooser{
   public static void main(String[] args) {
-    JChooser chooser;
+    JFileChooser chooser;
     File file, directory;
     int status;
-    chooser = new JChooser();
+    chooser = new JFileChooser();
     status = chooser.showOpenDialog(null);
 
-    if (status == JChooser.APPROVE_OPTION){
+    if (status == JFileChooser.APPROVE_OPTION){
       file = chooser.getSelectedFile();
       directory = chooser.getCurrentDirectory();
       System.out.println("Directory: " + directory.getName());
